b555e9dd63ea4e39ada539e9baf106c9a12646ab
JohnMaxwellGomes <johnmaxwellgomes@gmail.com>
2024-06-13 15:52:29 +0600

    First push

---
+++ Hands-on/1.1-terraform.tf
@@ -0,0 +1,162 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.33"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+# Create VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Create public subnet in AZ 1
+resource "aws_subnet" "public_az1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-west-2a"
+}
+
+# Create private subnet in AZ 1
+resource "aws_subnet" "private_az1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-west-2a"
+}
+
+# Create private subnet in AZ 2
+resource "aws_subnet" "private_az2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-west-2b"
+}
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Create a public route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Create a route for the public route table
+resource "aws_route" "public_route" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+# Associate the public subnets with the public route table
+resource "aws_route_table_association" "public_assoc_az1" {
+  subnet_id      = aws_subnet.public_az1.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Create a security group for the EC2 instance
+resource "aws_security_group" "ec2_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create a security group for the RDS instance
+resource "aws_security_group" "rds_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ec2_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create an EC2 instance
+resource "aws_instance" "web" {
+  ami                    = "ami-0cf2b4e024cdb6960"  # Specify your desired AMI
+  instance_type          = "t2.micro"
+  key_name               = "host"
+  subnet_id              = aws_subnet.public_az1.id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  tags = {
+    Name = "WebServer"
+  }
+}
+
+# Create an RDS instance
+resource "aws_db_instance" "default" {
+  allocated_storage    = 200
+  engine               = "mysql"
+  engine_version       = "8.0.35"
+  instance_class       = "db.m5.large"
+  identifier           = "mydb-instance"
+  username             = "admin"
+  password             = "password"
+  db_subnet_group_name = aws_db_subnet_group.default.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+
+  skip_final_snapshot = true
+}
+
+# Create a DB subnet group
+resource "aws_db_subnet_group" "default" {
+  name       = "main"
+  subnet_ids = [aws_subnet.private_az1.id, aws_subnet.private_az2.id]
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_id_az1" {
+  value = aws_subnet.public_az1.id
+}
+
+output "private_subnet_id_az1" {
+  value = aws_subnet.private_az1.id
+}
+
+output "private_subnet_id_az2" {
+  value = aws_subnet.private_az2.id
+}
+
+output "ec2_instance_id" {
+  value = aws_instance.web.id
+}
+
+output "rds_instance_endpoint" {
+  value = aws_db_instance.default.endpoint
+}
